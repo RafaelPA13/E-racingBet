e4350dbbaf773dbc396ebfb845edad583362e955
RafaelPA13 <rafaporann@gmail.com>
2024-09-23 18:02:40 -0300

    Adição dos cards dos circuitos

--- src/pages/Wiki/Wiki.jsx
+++ src/pages/Wiki/Wiki.jsx
@@ -1,31 +1,42 @@
-import "./Wiki.css";
-
+import Titulo from "../../components/Titulos/Titulos";
 import CardInfo from "../../components/CardInfo/CardInfo";
+import CardCircuito from "../../components/CardCircuito/CardCircuito";
+
+import circuitos from "../../data/circuitos.json";
+
+import "./Wiki.css";
 
 export default function Wiki() {
   return (
     <>
-      <h1>Wiki</h1>
+      <Titulo titulo={"Nossa História"} />
       <div className="container-info">
-        <CardInfo 
+        <CardInfo
           id="dark-blue"
           backgroundColor="var(--color-secondary)"
           title="O Início de Tudo"
           description="A Fórmula E, iniciada em 2014, é uma série de corridas de carros elétricos criada para promover a mobilidade sustentável e acelerar a adoção de veículos elétricos. A ideia foi desenvolvida por Jean Todt, da FIA, e Alejandro Agag em 2011. A primeira temporada contou com dez equipes e corridas em várias cidades globais."
         />
         <CardInfo
-          id="red" 
+          id="red"
           backgroundColor="var(--color-tertiary)"
           title="Modernização"
           description="Os carros eram inicialmente idênticos, mas a partir da segunda temporada, as equipes começaram a desenvolver seus próprios sistemas de propulsão. Em 2018, o carro Gen2 foi introduzido, eliminando a necessidade de trocas de carro durante as corridas. A Fórmula E também se destaca por sua interação com os fãs através do 'FanBoost'."
         />
-        <CardInfo 
+        <CardInfo
           id="light-blue"
           backgroundColor="var(--color-primary)"
           title="Consolidação"
           description="Grandes fabricantes como Audi, BMW, Mercedes-Benz, Jaguar e Porsche participam da competição. Em 2020, a Fórmula E foi reconhecida como um Campeonato Mundial pela FIA, consolidando seu prestígio. A série continua a ser uma plataforma para a inovação tecnológica e a sustentabilidade no automobilismo, promovendo um futuro mais verde."
         />
       </div>
+
+      <Titulo titulo={"Circuitos"} />
+      <ul className="lista-circuitos">
+        {circuitos.map((circuito) => (
+          <CardCircuito imagem={circuito.foto} nome={circuito.circuito} classe={circuito.classes} />
+        ))}
+      </ul>
     </>
   );
 }
