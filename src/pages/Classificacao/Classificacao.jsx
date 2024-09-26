2a4a2dc1b5d88d634f1d971a245b1d07ee6fd910
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-09-26 15:51:35 -0300

    página classificação estilizada  e componente cardpiloto responsivos

--- src/pages/Classificacao/Classificacao.jsx
+++ src/pages/Classificacao/Classificacao.jsx
@@ -1,12 +1,29 @@
+import "./Classificacao.css";
+
 import Titulos from "../../components/Titulos/Titulos";
-import CardPiloto from "../../components/CardPiloto/CardPiloto";
+import Input from "../../components/Input/Input";
+
 import pilotos from '../../data/pilotos.json'; 
+import CardPiloto from "../../components/CardPiloto/CardPiloto";
 
 export default function Classificacao() {
   return (
     <>
-      <Titulos titulo="Circuito" />
+      <Titulos titulo="Circuito: Berlim" />
       
+      <div className="circuito-container">
+        <img src="../../../public/circuitos/Circuito-Berlim.webp" alt="Circuito de Berlim" />
+      </div>
+
+      <div className="secao-container">
+        <h1>Classificação</h1>
+        <Input
+          icone={<i className="fas fa-search"></i>} 
+          tipo="text" 
+          placeholder="Buscar Pilotos:"
+        />  
+      </div>
+
       <div className="classificacao-container">
         {pilotos.map((piloto) => (
           <CardPiloto
