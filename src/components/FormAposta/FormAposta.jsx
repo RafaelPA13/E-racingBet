31f2715bcec652adffa55b26fd6b0f9aaaca324c
Jenismoraes <jenimoraeslima89@gmail.com>
2024-09-25 23:16:00 -0300

    add onChange

--- src/components/FormAposta/FormAposta.jsx
+++ src/components/FormAposta/FormAposta.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Input from '../Input/Input'; 
 import "./FormAposta.css";
 
-export default function ApostasForm({ categoria, pontos, icone, inputs }) {
+export default function ApostasForm({ categoria, pontos, icone, inputs, onChange }) {
   return (
     <div className="categorias">
-      <h1>{categoria}  {pontos} pts</h1>
+      <h1>{categoria} {pontos} pts</h1>
       {inputs.map((input, index) => (
         <div className="input-container" key={index}>
           <i className={icone}></i> 
           <Input 
             tipo="text" 
             value={input.value}
-            onChange={(e) => onChange(e.target.value)}
+            onChange={(e) => onChange(index, e.target.value)} 
             placeholder={input.placeholder} 
           />
         </div>
