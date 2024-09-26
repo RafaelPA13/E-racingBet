2a51cf3c15a326b6036d93acef8155ffd4f8da95
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-09-26 10:26:58 -0300

    adição do componente na página classificação e sua lista

--- src/pages/Classificacao/Classificacao.jsx
+++ src/pages/Classificacao/Classificacao.jsx
@@ -1,7 +1,24 @@
+import CardPiloto from "../../components/CardPiloto/CardPiloto";
+import pilotos from '../../data/pilotos.json'; 
+
 export default function Classificacao() {
   return (
     <>
       <h1>Classificação</h1>
+      <div className="classificacao-container">
+        {pilotos.map((piloto, index) => (
+          <CardPiloto
+            key={index}
+            posicao={index + 1}
+            nome={piloto.nome}
+            foto={piloto.foto}
+            equipe={piloto.equipe}
+            pontos={piloto.pontos}
+            melhorTempo={piloto.melhorTempo}
+            largada={piloto.largada}
+          />
+        ))}
+      </div>
     </>
   );
 }
