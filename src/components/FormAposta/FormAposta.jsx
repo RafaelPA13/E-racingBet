c03cbd1d934ed18f37b46e9c65c8be52b412d50b
Jenismoraes <jenimoraeslima89@gmail.com>
2024-09-26 21:14:41 -0300

    arrumando {index}

--- src/components/FormAposta/FormAposta.jsx
+++ src/components/FormAposta/FormAposta.jsx
@@ -6,13 +6,13 @@ export default function ApostasForm({ categoria, pontos, icone, inputs, onChange
   return (
     <div className="categorias">
       <h1>{categoria} {pontos} pts</h1>
-      {inputs.map((input, index) => (
-        <div className="input-container" key={index}>
+      {inputs.map((input) => (
+        <div className="input-container" key={input.id}>
           <i className={icone}></i> 
           <Input 
             tipo="text" 
             value={input.value}
-            onChange={(e) => onChange(index, e.target.value)} 
+            onChange={(e) => onChange(input.id, e.target.value)} 
             placeholder={input.placeholder} 
           />
         </div>
--- src/components/FormCadastro/FormCadastro.jsx
+++ src/components/FormCadastro/FormCadastro.jsx
@@ -3,8 +3,8 @@ import "./FormCadastro.css";
 export default function CadastroForm({ campos }) {
     return (
         <div>
-            {campos.map((campo, index) => (
-                <div className="input" key={index}>
+            {campos.map((campo) => (
+                <div className="input" key={campo.name}> 
                     <div className="icone">
                         <img src={campo.icone} alt={`${campo.placeholder} Icon`} />
                     </div>
